a6429bb7d96892ccecd23c48bf1d462b41b76642
jaopredo <joao.profissional.45@gmail.com>
2023-09-04 21:06:38 -0300

    Seeders Iniciais

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,6 @@ class Category extends Model
     use HasFactory;
 
     public function books() {
-        $this->hasMany(Book::class);
+        return $this->belongsToMany(Book::class);
     }
 }
