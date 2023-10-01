3b241cbdfb42b2155b90c570a768553468cb20a9
jaopredo <joao.profissional.45@gmail.com>
2023-09-30 22:21:52 -0300

    Refatoração para tailwind

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Acervo</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
     @bukStyles(true)
     @vite([
-        'resources/css/layout.css',
         'resources/css/app.css',
+        'resources/css/layout.css',
         'resources/css/floating.css',
         'resources/css/components/aside.css',
         'resources/css/components/action-buttons.css'
@@ -22,7 +20,7 @@
 <body>
     <div class="interface position-relative">
         <div class="filter-cover" style="display: none"></div>
-        <header class="header">
+        <header class="header bg-teal-500">
             <a href="/" class="site-brand">
                 <img src="{{ asset('img/logo-ceara.png') }}" alt="LOGO" class="logo">
             </a>
@@ -64,7 +62,6 @@
         @endif
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.min.js" integrity="sha512-hIdLfMv+eH9Ntu96uoHW9t0bMyiHRyKM8yQHZ09+th6TWagWlFonarbJfHrJb+me8wzaNnk1DVhqfOMwNKRq/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.js" integrity="sha512-OyVk2res2p8ZxK6Jf0Z6a9LhiQVn97RqQOF/G5p9rHm7W09tVg6wqbFG6TnDbLxyeEJb39PXXNsGQMmw1wlouQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
